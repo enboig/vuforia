7c7e883463cb2390594c32851459e0db82091f56
enBoig <enboig@gmail.com>
2021-05-05 15:40:03 +0200

    second

---
+++ Assets/scripts/doctor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doctor : MonoBehaviour
+{
+    float velocitat = 0f;
+
+    public GameObject visor = null;
+    public Transform target = null;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this.transform.Translate(new Vector3(1f, 0f, 0f));
+        // rb = gameObject.GetComponent<Rigidbody>();
+        //rb.velocity = new Vector3(Random.Range(5, 15), 0, Random.Range(5, 15));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            this.transform.Rotate(Vector3.up, -1);
+        }
+        else
+        {
+            this.transform.LookAt(target);
+
+        }
+    }
+}
---
+++ Assets/scripts/villager_on.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class villager_on : MonoBehaviour
+{
+    public GameObject visor = null;
+    public Transform target = null;
+    private GameObject figure=null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        figure.SetActive(false);
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
