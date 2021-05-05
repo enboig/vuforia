5a9746c7de9bb9972ba3c1d32dffa15897954aea
enBoig <enboig@gmail.com>
2021-05-05 17:20:51 +0200

    Colecció de ninots

---
+++ Assets/scripts/LoolCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoolCamera : MonoBehaviour
+{
+        public Transform target = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.LookAt(this.transform.parent);
+
+        // if (target == null)
+        // {
+        //     this.transform.Rotate(Vector3.up, -1);
+        // }
+        // else
+        // {
+        //     this.transform.LookAt(target);
+
+        // }
+    }
+}
---
+++ Assets/scripts/captured.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class captured: MonoBehaviour {
+  // float velocitat = 0f;
+
+  // public GameObject visor = null;
+  // public GameObject capturedObject = null;
+
+  // public int position;
+
+  // public Transform target = null;
+
+  // private Rigidbody rb;
+
+  // public bool visible = false;
+
+  
+
+  // Start is called before the first frame update
+  void Start() {
+//    this.GetComponent<MeshRenderer>().enabled = false;
+    gameObject.SetActive(false);
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    // if (this.visible == true) {
+    //     //fem que miri la càmara
+    //     this.transform.Rotate(Vector3.up, -1);
+    //     this.transform.LookAt(target);
+    // }
+  }
+
+  public void inFrontOfCamera() {
+      //this.GetComponent<MeshRenderer>().enabled = true;
+
+      gameObject.SetActive(true);
+      //this.transform.parent = camera.transform;
+
+  }
+}
